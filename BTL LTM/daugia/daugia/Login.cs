5868cac2074554cd2a65730213ac8a5e5edfd409
khoi2251120220 <146357980+khoi2251120220@users.noreply.github.com>
2024-10-13 15:43:44 +0700

    Add files via upload

---
+++ BTL LTM/daugia/daugia/Login.cs
@@ -0,0 +1,56 @@
+﻿
+namespace daugia
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SignUp signUpForm = new SignUp();
+            signUpForm.Show();
+            this.Hide();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userName = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (userName == "testUser" && password == "testPassword")
+            {
+                MessageBox.Show("Login successful!");
+                HomePage homePage = new HomePage();
+                homePage.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.");
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Mật khẩu đã được reset thành: 123a", "Reset Mật Khẩu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
