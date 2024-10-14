5aa87e50dbd6f62f7f4c67f72f008780458167ac
Nguyen Van Hoa <126589828+nguyenhoa-ize@users.noreply.github.com>
2024-10-14 16:57:00 +0700

    add package mysql

--- BTL LTM/daugia/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ BTL LTM/daugia/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab811242fdb41eb4d63db17e4cd23125bb0bf86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f57851f84dd77109e26c59ef33263220cb89e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
