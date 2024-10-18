7b919b57e3e7af9f4e1317c65328be89fde61df6
khoi2251120220 <146357980+khoi2251120220@users.noreply.github.com>
2024-10-18 20:16:29 +0700

    thêm socket tcp để login bằng thông tin server
    This reverts commit b17c0efcde4385a9961039d559af04c2350d7cc9.

--- BTL LTM/daugia/daugia/Program.cs
+++ BTL LTM/daugia/daugia/Program.cs
@@ -1,53 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Sockets;
+using System.Windows.Forms;
 
-namespace Client
+namespace daugia
 {
-    class Program
+    static class Program
     {
-        private const int BUFFER_SIZE = 1024;
-        private const int PORT_NUMBER = 9999;
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
-            Console.OutputEncoding = Encoding.Unicode;
-            Console.InputEncoding = Encoding.Unicode;
-
-            try
-            {
-                TcpClient client = new TcpClient();
-
-                client.Connect("127.0.0.1", PORT_NUMBER);
-                NetworkStream stream = client.GetStream();
-
-                Console.WriteLine("Đã kết nối với Server.");
-                Console.Write("Tên của bạn là gì? ");
-
-                string str = Console.ReadLine();
-
-                byte[] data = Encoding.UTF8.GetBytes(str);
-
-                stream.Write(data, 0, data.Length);
-
-                data = new byte[BUFFER_SIZE];
-                stream.Read(data, 0, BUFFER_SIZE);
-
-                Console.WriteLine(Encoding.UTF8.GetString(data));
-
-                stream.Close();
-                client.Close();
-            }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine("Lỗi: " + ex);
-            }
-
-            Console.Read();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login()); 
         }
     }
 }
--- BTL LTM/daugia/daugia/obj/Debug/net8.0-windows/client.AssemblyInfo.cs
+++ BTL LTM/daugia/daugia/obj/Debug/net8.0-windows/client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa982ce664832b854b1ab7eacf6562ca365e4aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b17c0efcde4385a9961039d559af04c2350d7cc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
