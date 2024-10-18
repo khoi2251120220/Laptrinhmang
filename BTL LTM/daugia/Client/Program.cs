88c9b9bd3587cfff7769e56f31bc08d161edd3ee
ndang <ndang2319@gmail.com>
2024-10-19 05:13:10 +0700

    add Client for test

---
+++ BTL LTM/daugia/Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Shared.Models;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketClient client = new SocketClient("127.0.0.1", 8888);
+
+            Console.WriteLine("Connected to server. Requesting auctions...");
+
+            string response = client.SendRequest("GET_AUCTIONS");
+            List<Auction> auctions = JsonSerializer.Deserialize<List<Auction>>(response);
+
+            Console.WriteLine("Current auctions:");
+            foreach (var auction in auctions)
+            {
+                Console.WriteLine($"{auction.Id} | {auction.PlateNumber} | {auction.CurrentPrice} | {auction.Status} | {auction.HighestBidder}");
+            }
+
+            client.Close();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
